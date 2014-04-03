c9f4713de85cb323e28931dd7925446b5b17d262
nick <thedeadofblackfire@yahoo.com>
2014-04-03 21:59:45 +0200

    03042200

--- www/js/textsol.js
+++ www/js/textsol.js
@@ -929,7 +929,7 @@ function generatePageSession(data) {
 	str += '<div class="chat-footer chatform">';
     str += '<input type="text" data-session="'+data.session_id+'" name="chatText" id="chatInput" class="input-light input-large brad chat-search" placeholder="'+i18n.t('label.pressenter')+'">';
     //str += '<a data-role="button" href="#" data-session="'+data.session_id+'" class="btn btn-primary btnChatSendReply">'+i18n.t('label.send')+'</a>';
-    str += '<a data-role="button" href="#" data-icon="arrow-r" data-iconpos="right" data-session="'+data.session_id+'" class="btnChatSendReply" data-i18n="label.send">'+i18n.t('label.send')+'</a>';  
+    str += '<a href="#" data-session="'+data.session_id+'" class="ui-btn ui-icon-arrow-r ui-btn-icon-right ui-shadow ui-corner-all btnChatSendReply" data-i18n="label.send">'+i18n.t('label.send')+'</a>';  
     str += '</div>';				
         
     str += '</div>';
